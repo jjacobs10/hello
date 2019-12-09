6852a05e74cfd3ef761a8615f6ff3f22f0ba0734
jjacobs10 <jjacobs10@huskers.unl.edu>
2019-12-09 15:59:19 -0600

    Update hello.c
    Since this is a redo assignment, I am unable to get the original collaborators to push to my repository again.

--- hello.c
+++ hello.c
@@ -1,5 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 int main(int argc, char **argv) {
   printf("Joshua Jacobs\n");
+  printf("Computer Science\n");
+
+  printf("Brandon Peters\n");
+  printf("Computer Science\n")
+
+  printf("Logan Puatu\n");
   printf("Computer Science\n");
 
   return 0;
